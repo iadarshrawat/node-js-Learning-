e96574eef619d983979790fa471f1824585c5583
adarsh rawat <00adarshrawat00@gmail.com>
2023-03-02 18:37:27 +0530

    second commit

--- fs-module.js
+++ fs-module.js
@@ -1,1 +1,21 @@
-console.log("hello")
+    const fs = require('fs')
+    // Read files
+    // Create files
+    // Update files
+    // Delete files
+    // Rename files
+
+    // creating a file 
+    fs.appendFile('text.txt',"this is my text file",(err)=>{ // add text in existing file
+        if(err) throw err
+        console.log('success')
+    })
+    fs.writeFile('text2.txt',"this is my text file",(err)=>{ // replace text in existing file
+        if(err) throw err
+        console.log('success')
+    })
+
+
+
+
+    
